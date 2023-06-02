e9eabcea3038b26b7e90e1be5bce214d8a032432
Siddhant Madhur <siddhant.madhur@gmail.com>
2023-06-02 13:26:24 +0400

    Initial Commit

---
+++ TaskPro/TaskProApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskProApp.swift
+//  TaskPro
+//
+//  Created by Siddhant Madhur on 02/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct TaskProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
